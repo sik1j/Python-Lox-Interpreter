e12917c4698ebc41120f8e53672df8165dfb16b7
sik1j <karkisikij2@gmail.com>
2023-11-13 10:38:38 -0800

    Added comments to token.py

--- token.py
+++ token.py
@@ -1,11 +1,13 @@
 from tokenType import TokenType
 
-
+# represents Lox tokens
 class Token:
     def __init__(self, token_type: TokenType, lexeme: str, literal: object, line: int):
         self.type = token_type
         self.lexeme = lexeme
+        # numbers, strings, and identifiers
         self.literal = literal
+        # line no. token found on
         self.line = line
 
     def __str__(self):
